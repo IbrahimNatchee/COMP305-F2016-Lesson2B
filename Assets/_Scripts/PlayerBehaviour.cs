f94687901ad7a1d1e848e1cd72e077750b5af047
Abraham <ibrahim.natchee@outlook.com>
2016-10-03 15:48:49 -0700

    it's Moving

--- Assets/_Scripts/PlayerBehaviour.cs
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -18,11 +18,10 @@ public class PlayerBehaviour : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         Rotation();
-        //Movement();
+        Movement();
 }
 
-void Rotation()
-    {
+void Rotation(){
         Vector3 worldPos = Input.mousePosition;
         worldPos = Camera.main.ScreenToWorldPoint(worldPos);
         float dx = this.transform.position.x - worldPos.x;
@@ -30,5 +29,25 @@ void Rotation()
         float angle = Mathf.Atan2(dy, dx) * Mathf.Rad2Deg;
         Quaternion rot = Quaternion.Euler(new Vector3(0, 0, angle + 90));
         this.transform.rotation = rot;
+    }
+
+    void Movement() {
+        Vector3 movement = new Vector3();
+        movement.x += Input.GetAxis("Horizontal");
+        movement.y += Input.GetAxis("Vertical");
+
+        movement.Normalize();
+
+        if (movement.magnitude > 0) {
+            currentSpeed = playerSpeed;
+            this.transform.Translate(movement * Time.deltaTime * playerSpeed, Space.World);
+            lastMovement = movement;
+        }
+        else
+        {
+            
+            this.transform.Translate(lastMovement * Time.deltaTime * currentSpeed, Space.World);
+            lastMovement *= .9f;
+        }
     }
 }
